08f9a282d176c39df6637670338f908bf4d90756
Ekta Gogri <egogri@usc.edu>
2020-08-03 16:25:05 -0700

    Update twilioService.swift

--- FinalProjectGogriEkta/Model/twilioService.swift
+++ FinalProjectGogriEkta/Model/twilioService.swift
@@ -11,15 +11,16 @@ import Alamofire
 
 class TwilioService{
    static let shared = TwilioService()
-    
-   private let accountSID:String = "ACa4703f086b600d757d4a65a9ff5d6c0b"
-   private let authToken:String = "8a4932d6b384dfad782c2fb60243b3ea"
-   private let twilioNumber:String = "+17243906696"
+   
+   //values removed
+   private let accountSID:String = ""
+   private let authToken:String = ""
+   private let twilioNumber:String = ""
    private let numberToMessage:String = ""
     //send message to detected card user
     func sendMessage(phoneNumber:String) {
         let url = "https://api.twilio.com/2010-04-01/Accounts/\(accountSID)/Messages"
-        let parameters = ["From": "\(twilioNumber)", "To": "+16692613312", "Body": "Hello! Let's connect :)"]
+        let parameters = ["From": "\(twilioNumber)", "To": "", "Body": "Hello! Let's connect :)"]
 
         Alamofire.request(url, method: .post, parameters: parameters)
             .authenticate(user: accountSID, password: authToken)
